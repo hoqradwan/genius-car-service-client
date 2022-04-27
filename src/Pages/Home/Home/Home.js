e8d93e6d24d04ae4655eff37a59c6c02214fdc0f
hoqradwan <92720128+hoqradwan@users.noreply.github.com>
2022-04-27 20:03:20 +0600

    cliet side added

---
+++ src/Pages/Home/Home/Home.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Helmet } from "react-helmet-async";
+import PageTitle from "../../Shared/PageTitle/PageTitle";
+import Banner from "../Banner/Banner";
+import Experts from "../Experts/Experts";
+import Services from "../Services/Services";
+
+const Home = () => {
+  return (
+    <>
+      <PageTitle title="Home"></PageTitle>
+
+      <Banner></Banner>
+      <Services></Services>
+      <Experts></Experts>
+    </>
+  );
+};
+
+export default Home;
